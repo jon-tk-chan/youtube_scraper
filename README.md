dacc4e5bc6e240b5424f4c58ff8d4e662e013c37
jon-tk-chan <jonathan.chan41@gmail.com>
2023-07-26 09:59:46 -0700

    refactored code, readme rough details

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # youtube_scraper
-Python script for scraping Youtube video statistics and transcript text
+Python script for scraping Youtube video statistics and transcript text.
+
+Saves a file containing video statistics (view count, channel title, )
+
+Requires:
+- Google API Python client and Youtube Transcript API - install using `pip install -r requirements.txt`
+
+Resources:
+[Youtube Data API Overview](https://developers.google.com/youtube/v3/getting-started)
+[Sudharsan Asaithambi - Youtube Data in Python (Medium Article)](https://medium.com/greyatom/youtube-data-in-python-6147160c5833)
+[Python - Downloading captions from Youtube](https://www.geeksforgeeks.org/python-downloading-captions-from-youtube/)
---
+++ app.py
@@ -0,0 +1,198 @@
+# from apiclient.discovery import build
+from googleapiclient.discovery import build
+from youtube_transcript_api import YouTubeTranscriptApi
+# from apiclient.errors import HttpError
+# from oauth2client.tools import argparser
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# # import ytcreds
+import json
+import credentials
+DEVELOPER_KEY = credentials.API_KEY
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
+
+search_query="why I quit my job"
+total_videos = 50
+
+def get_video_metadata(query="Why I quit my job", results_total=250):
+    """Returns a list of results_total number of video responses
+        Each item returned is from youtube.search().list(q=query)
+        Section 1 - function iterates through pages of responses (max number per page = 50)
+        Section 2 - function iterates through pages of responses and gets relevant fields: 
+            videoId, videoTitle, channelId, publishedAt
+            
+        input:
+            query: str
+            results total: int of number of videos to do
+    """
+    
+    ##SECTION 1 - api call to get lists of functions
+    responses_list =[] #list of pages of responses
+    results_total += 1
+    for i in range(0,results_total,50):
+        # if i % 100 == 0:
+        #     print(f"VIDEOS PROCESSED: {i}")
+        if i ==0:
+            next_token=None
+            prev_token = None
+            
+        #Call youtube.search() command using default values - getting id and snippet parts of youtube info
+        search_response = youtube.search().list(
+            q=query, type="video", pageToken=next_token,
+            order = 'relevance',part="id,snippet",maxResults=results_total,
+            location=None, locationRadius=None).execute()
+        responses_list.append(search_response)
+
+        #Get next page and previous page of videos, update tokens
+        if 'nextPageToken' in search_response.keys():
+            next_token=search_response['nextPageToken']
+        else:
+            next_token=None
+        if 'prevPageToken' in search_response.keys():
+            prev_token=search_response['prevPageToken']
+        else:
+            prev_token=None
+    ##SECTION 2 - create list of ids to iterate through - remove the layer of lists given for individual queries of youtube.search()
+    video_list = []
+    for response in responses_list:
+        videos_list = response['items']
+        for item in videos_list:
+            video_title=item['snippet']['title']
+    #         desc = item['snippet']['description'] #only gives snippet of desc - add when getting transcripts
+            video_id = item['id']['videoId']
+            channel_id = item['snippet']['channelId']
+            published_at = item['snippet']['publishedAt']
+            video_dict = {"videoId": video_id,
+                "videoTitle": video_title,
+                "channelId": channel_id,
+                'publishedAt': published_at}
+            video_list.append(video_dict)
+    # print(f"COMPLETED - NUMBER OF VIDEO RESPONSES: {len(video_list)}")
+    return video_list
+def get_video_stats(video_list):
+    """Return a dictionary of lists containing the info for each video in video_list"""
+    title = []
+    channelId = []
+    channelTitle = []
+    categoryId = []
+    publishedAt=[]
+    videoId = []
+    viewCount = []
+    likeCount = []
+    dislikeCount = []
+    commentCount = []
+    favoriteCount = []
+    category = []
+    tags = []
+    videos = []
+    descriptions=[]
+    i = 0
+    for video in video_list:
+        # if i % 100 == 0:
+        #     print("VIDEOS PROCESSED: ", i)
+        videoId.append(video['videoId'])
+        title.append(video['videoTitle'])
+        publishedAt.append(video['publishedAt'])
+        stats = youtube.videos().list(
+            part='statistics, snippet',
+            id=video['videoId']).execute()
+        #values for each video stored for <GIVEN FIELD>: 
+        #stats['items'][0]['statistics'][<GIVEN FIELD>] OR stats['items'][0]['snippet'][<GIVEN FIELD>]
+        channelId.append(stats['items'][0]['snippet']['channelId']) 
+        channelTitle.append(stats['items'][0]['snippet']['channelTitle']) 
+        categoryId.append(stats['items'][0]['snippet']['categoryId']) 
+        favoriteCount.append(stats['items'][0]['statistics']['favoriteCount'])
+        #fill remaining lists from stats['items'][0]['statistics'] OR stats['items'][0]['snippet']
+        try:
+            viewCount.append(stats['items'][0]['statistics']['viewCount']) 
+        except:
+            viewCount.append("Not available") 
+        try:
+            likeCount.append(stats['items'][0]['statistics']['likeCount'])
+        except:
+            likeCount.append("Not available")
+        try:
+            dislikeCount.append(stats['items'][0]['statistics']['dislikeCount'])     
+        except:
+            dislikeCount.append("Not available")
+        if 'commentCount' in stats['items'][0]['statistics'].keys():
+            commentCount.append(stats['items'][0]['statistics']['commentCount'])
+        else:
+            commentCount.append(0)
+        if 'tags' in stats['items'][0]['snippet'].keys():
+            tags.append(stats['items'][0]['snippet']['tags'])
+        else:
+            tags.append("No Tags")
+        if 'description' in stats['items'][0]['snippet'].keys():
+            descriptions.append(stats['items'][0]['snippet']['description'])
+        else:
+            tags.append("No Description")
+        #a given video is equivelant to stats['items'][0]
+        i += 1
+
+    #write JSON to store video information for all
+    youtube_dict = {'tags':tags,'channelId': channelId,'channelTitle': channelTitle,
+                    'categoryId':categoryId,'publishedAt': publishedAt,'title':title,'videoId':videoId,
+                    'viewCount':viewCount,'likeCount':likeCount,'dislikeCount':dislikeCount,
+                    'commentCount':commentCount,'favoriteCount':favoriteCount,
+                   "description": descriptions}
+    # print(f"DONE - NUMBER OF VIDEOS PROCESSED: {len(youtube_dict['channelId'])}")
+    return youtube_dict
+
+def add_youtube_transcripts(youtube_dict):
+    """return a copy of youtube_dict with list of transcript strings and 
+    list of raw transcripts (list of list of cleaned strings)
+    """
+    final_dict = youtube_dict.copy()
+    transcripts_sents = []
+    transcripts_strings = []
+    transcripts_parsed = []
+    for i, video_id in enumerate(youtube_dict['videoId']):
+#         print(video_id)
+        transcript_sentlist = []
+        #use YouTubeTranscriptApi.get_transcript() and write "NONE" if not
+        try:
+            eng_transcript = YouTubeTranscriptApi.get_transcript(video_id,languages=['en'])
+            transcript_sentlist = [str(x['text']).replace("\xa0", "") for x in eng_transcript]
+            transcript_joined = " ".join(transcript_sentlist)
+        except:
+            # print("NONE")
+            transtript_sentlist = None
+            transcript_joined = None
+        transcripts_sents.append(transcript_sentlist)
+        transcripts_strings.append(transcript_joined)
+    final_dict['transcripts_raw'] = transcripts_sents
+    final_dict['transcript_strings'] = transcripts_strings
+    # print(f"DONE - NUMBER OF VIDEOS PROCESSED: {len(final_dict['transcript_strings'])}")
+    return final_dict
+
+
+def main():
+    """main executable function - calls scraper functions in order, saves to predefined filepath"""
+    print(f"start - collecting urls for given search query: {search_query}")
+    video_list = get_video_metadata(search_query,total_videos)
+    print(f"Scraped number of videos: {len(video_list)}")
+    youtube_dict = get_video_stats(video_list)
+    print(f"Scraped stats for videos: {len(youtube_dict['channelId'])}")
+    print("collecting transcripts - time intensive task")
+    try:
+        final_dict = add_youtube_transcripts(youtube_dict)
+        print(f"Scraped transcripts for videos: {len(final_dict['transcript_strings'])}")
+
+        # print("FIRST 5 of each column")
+        # for key, dict_items in final_dict.items():
+        #     # dict_item = final_dict[key][0:5]
+        #     dict_examples = dict_items[0:5]
+        #     print(f"************** {key} *********************")
+        #     print(dict_examples)
+        #     print(type(dict_examples))
+        #     print("----")
+        filename="data/youtube_stats_and_transcripts.json"
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(final_dict, f, ensure_ascii=False, indent=4)
+    except:
+        print("issue - add_youtube_transcripts()")
+if __name__ == "__main__":
+    main()
